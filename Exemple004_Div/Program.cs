634b73818e8b91fddc839f4e1b255358a7ddd107
Alena <gertmanea@mail.ru>
2022-06-24 18:41:52 +0300

    Exe004

---
+++ Exemple004_Div/Program.cs
@@ -0,0 +1,9 @@
+﻿int NumberA = new Random().Next(1, 20);
+Console.WriteLine(NumberA);
+
+int NumberB = new Random().Next(1, 20);
+Console.WriteLine(NumberB);
+
+int result = (NumberA + NumberB);
+Console.WriteLine(result);
+
